0bc2affcf753b92bcafa1043223b1e5654e0c940
ketan0608 <76109939+ketan0608@users.noreply.github.com>
2022-05-13 22:22:04 +0530

    Add files via upload

---
+++ p-102.py
@@ -0,0 +1,18 @@
+import cv2
+import time
+import random
+def take_snapshot():
+    number=random.randint(0,100)
+    videoCaptureObject=cv2.VideoCapture(0)
+    result=True
+    while(result):
+        ret,frame=videoCaptureObject.read()
+        img_name="img"+str(number)+".png"
+        cv2.imwrite("NewPicture1.jpg",frame)
+        result=False
+    print('Snapshot Taken')
+    return img_name
+    print('snapshot stored')
+    videoCaptureObject.release()
+    cv2.destroyAllWindows()
+take_snapshot()
